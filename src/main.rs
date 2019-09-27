9516ac5d66c082edf396bec2bbdb6189896ac65b
Calum <calumforster@live.co.uk>
2019-09-27 21:50:45 +0100

    Create proper library.

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,8 @@
-extern crate qrcode;
-extern crate terminal_graphics;
+extern crate qr_terminal;
 
-use qrcode::QrCode;
-use terminal_graphics::Display;
-use terminal_graphics::Colour;
+use qr_terminal::TermQrCode;
 
 fn main() {
-    let code = QrCode::new(b"Hello, World!").unwrap();
-    let width = code.width();
-    let height = (width as f32/2 as f32).ceil() as usize;
-    let pixels = code.to_colors();
-    let mut display = Display::new((width + 2) as u32, (height + 1) as u32);
-    
-    for (i, pixel) in pixels.iter().enumerate() {
-        let x = i % width;
-        let y = (i as f32/width as f32).floor() as isize;
-
-        let colour = match pixel {
-            qrcode::types::Color::Light => Colour::White,
-            qrcode::types::Color::Dark => Colour::Black,
-        };
-
-        let char_colour = match width % 2 {
-            0 => colour,
-            1 => Colour::White,
-            _ => Colour::White,
-        };
-
-        match y % 2 {
-            0 => display.set_pixel(x as isize + 1, (y/2) as isize + 1, '▄', char_colour, colour),
-            1 => display.get_mut_pixel(x as isize + 1, ((y-1)/2) as isize + 1).set_colour(colour),
-            _ => println!("That shouldn't happen"),
-        }
-    }
-
-    for i in 0..((height + 2) as u32) {
-        println!();
-    }
-
-    display.print();
+    let code = TermQrCode::from_bytes(b"Hello, World!");
+    code.print();
 }
